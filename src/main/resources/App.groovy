c5ebcdab1db5946ff01b8d7173012ff17b78391b
Olaf Geibig <olaf.geibig@tutaona.com>
2014-05-28 13:40:35 +0200

    removed unused stuff

--- src/main/resources/App.groovy
+++ src/main/resources/App.groovy
@@ -1,5 +1,30 @@
+def config = container.config
+
 container.with {
     deployVerticle('WebServer.groovy')
     deployVerticle('UrbanAirshipClient.groovy')
     deployVerticle('TaggingEngine.groovy')
+
+    // Deploy a MongoDB persistor module
+    deployModule('io.vertx~mod-mongo-persistor~2.1.0', config.'mongo-persistor') { asyncResult ->
+        if (asyncResult.succeeded) {
+            // And when it's deployed run a script to load it with some reference
+            // data for the demo
+            //deployVerticle('WebServer.groovy')
+            container.logger.info "Successfully connected to MongoDB"
+        } else {
+            container.logger.error "Failed to deploy mongo-persistor ${asyncResult.throwable}"
+        }
+    }
+
+    deployModule('io.vertx~mod-mysql-postgresql~0.3.0-SNAPSHOT', config.'mysql-postgresql') { asyncResult ->
+        if (asyncResult.succeeded) {
+            // And when it's deployed run a script to load it with some reference
+            // data for the demo
+            //deployVerticle('WebServer.groovy')
+            container.logger.info "Successfully connected to PostgreSQL DB"
+        } else {
+            container.logger.error "Failed to deploy mysql-postgresql ${asyncResult.throwable}"
+        }
+    }
 }
--- src/main/resources/WebServer.groovy
+++ src/main/resources/WebServer.groovy
@@ -4,150 +4,68 @@ import org.vertx.groovy.core.http.RouteMatcher
 import org.vertx.groovy.core.http.impl.DefaultHttpServer
 import org.vertx.groovy.platform.Verticle
 import groovy.json.JsonSlurper
+import groovy.json.JsonBuilder
+import org.vertx.java.core.json.JsonObject
+import org.vertx.java.core.json.JsonArray
+import org.vertx.groovy.core.AsyncResult
 
 RouteMatcher routeMatcher = new RouteMatcher()
 def eventBus = vertx.eventBus
 def log = container.logger
 
-// generic get all
-routeMatcher.get('/internal/job/:entity/') { request ->
-    def auth = request.headers.Authorization
-
-    if(!auth) {
-        log.info(auth)
-        request.response.statusCode = 401
-        request.response.statusMessage = 'Unauthorized'
-        request.response.end('Unauthorized')
-    } else {
-        log.info(auth)
-        String entity = request.params['entity']
-    eventBus.send("UrbanAirship.${entity}.getAll", '') { message ->
-        //log.info "${System.currentTimeMillis()} ${message.body}"
-        def json = new JsonSlurper().parseText(message.body)
-        request.response.putHeader('Content-Type', 'application/json; charset=utf-8')
-        request.response.statusCode = json.statusCode
-        request.response.statusMessage = json.statusMessage
-        def body = ''
-        if(request.response.statusCode == 200) {
-            body = new JsonSlurper().parseText(json.content)
-        } else {
-            body = json.content
-        }
-        def responseJson = new JsonBuilder()
-        responseJson {
-            statusCode json.statusCode
-            statusMessage json.statusMessage
-            content body
-        }
-        request.response.end(responseJson.toString())
-    }}
-}
-
-// generic create
-routeMatcher.post('/internal/job/:entity/') { request ->
-    String entity = request.params['entity']
-    def body = new Buffer(0)
-    request.bodyHandler { buffer ->
-        body.appendBuffer(buffer as Buffer)
-        eventBus.send('UrbanAirship.${entity}.create', body.toString()) { message ->
-            log.info "${System.currentTimeMillis()} ${message.body}"
-            def json = new JsonSlurper().parseText(message.body)
-            request.response.statusCode = json.statusCode
-            request.response.statusMessage = json.statusMessage
-            request.response.end(json.statusMessage)
-        }
-    }
-}
-
-// generic delete
-routeMatcher.delete('/internal/job/:entity/:id') { request ->
-    String entity = request.params['entity']
-    eventBus.send("UrbanAirship.${entity}.delete", request.params.id) { message ->
-        //log.info "${System.currentTimeMillis()} ${message.body}"
-        def json = new JsonSlurper().parseText(message.body)
-        request.response.statusCode = json.statusCode
-        request.response.statusMessage = json.statusMessage
-        request.response.end(json.statusMessage)
-    }
-}
-
-// create tag
-routeMatcher.put('/internal/job/tag/:tag') { request ->
-    eventBus.send('UrbanAirship.tag.create', request.params.tag) { message ->
-        //log.info "${System.currentTimeMillis()} ${message.body}"
-        def json = new JsonSlurper().parseText(message.body)
-        request.response.statusCode = json.statusCode
-        request.response.statusMessage = json.statusMessage
-        request.response.end(json.statusMessage)
-    }
-}
-
-routeMatcher.post('/internal/job/query/exec') { request ->
-    def body = new Buffer(0)
-    request.bodyHandler { buffer ->
-        body.appendBuffer(buffer as Buffer)
-        log.info(body.toString())
-        eventBus.send('TaggingEngine.query.exec', body.toString()) { message ->
-            //log.info "${System.currentTimeMillis()} ${message.body}"
-            def json = new JsonSlurper().parseText(message.body)
-            request.response.statusCode = json.statusCode
-            request.response.statusMessage = json.statusMessage
-            request.response.end(message.body.toString())
-        }
-    }
-}
-
-routeMatcher.post('/internal/job/query/sample') { request ->
-    def body = new Buffer(0)
-    request.bodyHandler { buffer ->
-        body.appendBuffer(buffer as Buffer)
-        eventBus.send('TaggingEngine.query.exec', body.toString()) { message ->
-            //log.info "${System.currentTimeMillis()} ${message.body}"
-            def json = new JsonSlurper().parseText(message.body)
-            request.response.statusCode = json.statusCode
-            request.response.statusMessage = json.statusMessage
-            request.response.end(message.body.toString())
-        }
-    }
-}
-
-routeMatcher.post('/internal/job/pushNotification') { request ->
-    def body = new Buffer(0)
-    request.bodyHandler { buffer ->
-        body.appendBuffer(buffer as Buffer)
-        eventBus.send('UrbanAirship.push', body.toString()) { message ->
-            log.info "${System.currentTimeMillis()} ${message.body}"
-            def responseJson = new JsonSlurper().parseText(message.body)
-            request.response.statusCode = responseJson.statusCode
-            request.response.statusMessage = responseJson.statusMessage
-            request.response.end(message.body.toString())
-        }
-    }
-}
-
-routeMatcher.get('/internal/job/foo') { request ->
-    def auth = request.headers.Authorization
-
-    if(!auth) {
-        log.info(auth)
-        request.response.statusCode = 401
-        request.response.statusMessage = 'Unauthorized'
-        request.response.end('Unauthorized')
-    } else {
-        auth -= 'Basic '
-        def user = new String(javax.xml.bind.DatatypeConverter.parseBase64Binary(auth)).split(':')[0]
-        log.info("$auth ${user}")
-        if(user != 'foo') {
-            request.response.statusCode = 401
-            request.response.statusMessage = 'Unauthorized'
-            request.response.end('Unauthorized')
-        } else {
+routeMatcher.get('/audit') { request ->
+    def limit = request.params.pageSize as int
+    def offset = ((request.params.page as int) - 1) * limit
+    def json = [
+            'action':'prepared',
+            'statement':'SELECT * FROM push_audit LIMIT ? OFFSET ?',
+            'values':[limit, offset]
+    ]
+    def jsonMsg = new JsonObject(json)
+    eventBus.send('postgresql', jsonMsg) { message ->
+        if(message.body.status == 'ok') {
             request.response.statusCode = 200
-            request.response.statusMessage = 'OK'
-            request.response.end('OK')
+        } else {
+            request.response.statusCode = 500
         }
+        def audits = []
+        message.body.results.each { row ->
+            audits << [id:row[0], jobId:row[1], action:row[2], username:row[3], status:row[4], created:row[5],
+                    request:row[6], response:row[7]]
+        }
+        request.response.putHeader('Content-Type', 'application/json; charset=utf-8')
+        request.response.statusMessage = message.body.status
+        request.response.putHeader('Content-Type', 'application/json; charset=utf-8')
+        request.response.end(new JsonArray(audits, false).toString())
     }
 }
+
+routeMatcher.get('/audit/:id') { request ->
+    def id = request.params.id as long
+    def json = [
+            'action':'prepared',
+            'statement':'SELECT * FROM push_audit WHERE id = ?',
+            'values':[id]
+    ]
+    def jsonMsg = new JsonObject(json)
+    eventBus.send('postgresql', jsonMsg) { message ->
+        if(message.body.status == 'ok') {
+            request.response.statusCode = 200
+        } else {
+            request.response.statusCode = 500
+        }
+        def audits = []
+        message.body.results.each { row ->
+            audits << [id:row[0], jobId:row[1], action:row[2], username:row[3], status:row[4], created:row[5],
+                    request:row[6], response:row[7]]
+        }
+        request.response.putHeader('Content-Type', 'application/json; charset=utf-8')
+        request.response.statusMessage = message.body.status
+        request.response.putHeader('Content-Type', 'application/json; charset=utf-8')
+        request.response.end(new JsonArray(audits, false).toString())
+    }
+}
+
 
 // serve all files from directory jobs
 routeMatcher.getWithRegEx("^\\/internal/jobs\\/.*") { req ->
@@ -160,4 +78,4 @@ server.requestHandler(routeMatcher.asClosure())
 vertx.createSockJSServer(server).bridge(prefix: '/eventbus', [[:]], [[:]])
 
 server.listen(8080)
-container.logger.info("Jobs webserver running!")
+container.logger.info("webserver running!")
